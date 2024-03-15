9da25403d63c8ccd5e4420dca39fd6ba7355089c
Alexey Frolov <63521003+catpital@users.noreply.github.com>
2024-03-16 01:20:25 +0300

    weather added {}

--- headlers/weather.py
+++ headlers/weather.py
@@ -1,25 +1,27 @@
 from aiogram import types, F, Router
+from aiogram.filters.command import Command
 import requests
 from utilits import weath
 from aiogram.filters.command import Command
 #from aiogram.fsm.context import FSMContext
 #from aiogram.fsm.state import State, StatesGroup
-#from keyboard.prof_keyboards import make_row_keyboard
+from keyboard.prof_keyboards import make_row_keyboard
 
-weather_names=['/Погода', 'Инфо']
+weather_names = ['Погода в СПб', 'Погода в Москве', 'Инфо']
 
 
 router = Router()
 #class ChoiseProfName(StatesGroup):
-#   choise_prof_name = State()
-#    choise_prof_graders = State()
+#   choise_city_name = State()
+#   переменная для выбора города
 
-@router.message(F.text.lower() == 'погода')
+@router.message(Command('weather'))
 async def mes_weather(message: types.Message):
   #  name = message.chat.first_name
     temp = weath.weathers()
-    await message.answer(f'Погода нынче такая  {temp}')
-@router.message(Command('weather'))
+    city = weath.weathers()
+    await message.answer(f'Погода нынче в городе {city} такая {temp}', reply_markup=make_row_keyboard(weather_names))
+@router.message(F.text.lower() == 'погода')
 async def cmd_weather(message: types.Message):
   #  name = message.chat.first_name
     await message.answer(f'Now weather is cold')
--- utilits/weath.py
+++ utilits/weath.py
@@ -1,29 +1,30 @@
 import requests
 import asyncio
-import json
+import gson
 import config
 def weathers():
     weather_key = config.weather_key
-    #id: 14,cityid: '498817',appid: 'cfcfe73689ce00355c0d2a157b211d4c',units: 'metric'
-    #url = 'http://api.openweathermap.org/data/2.5/weather?q=Moskow&APPID={weather_key}'
-    #http://api.openweathermap.org/data/2.5/weather?q=москва&lang=ru&units=metric&APPID=cfcfe73689ce00355c0d2a157b211d4c
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=москва&lang=ru&units=metric&callback=test&appid={weather_key}'
+
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=москва&lang=ru&units=metric&appid={weather_key}'
     response = requests.get(url)
 
     if response.status_code:
-        data = response.json()
+        wdata = response.json()
         #description = data['weather'][0]['description']
         #['main]['humidity]
-        #city=data['name']
+        city=wdata.get('name', {})
         #pressure = data['main']['pressure']
         #wind = data['wind']['speed']
         #return (cur_temp)
-
-        temp = int(data["main"]["temp"] - 273.15)  # convert from Kelvin to Celsius
+        #idweather=data['weather']['id']
+        #temp=wdata.get('weather', {}).get('id')
+        temp = wdata.get('main', {}).get('temp')   #  Celsius
        # return temp
+      #  async def cmd_weath_ans(message: types.Message):
+      # await message.answer(f'Weather , {temp}')
     else:
-        temp=0
-    return temp
+        temp="null"
+    return temp, city
 
 if __name__ == '__main__':
         weathers()
